d6a568daf53cd239e06ad340cc54aca9e2961fd9
Mohak Tandon <er.mohak97@gmail.com>
2022-12-16 10:32:40 +0530

    day-3

---
+++ tripadvisor/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import {HStack,Box,Image} from "@chakra-ui/react";
+
+<HStack>
+  <Box>
+  <Image src="https://static.tacdn.com/img2/brand_refresh/Tripadvisor_lockup_horizontal_secondary_registered.svg"></Image>
+  </Box>
+</HStack>
