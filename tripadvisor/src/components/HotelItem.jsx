0898bf16102215b7e3e9f091ae9d028c781c5ea4
Mohak Tandon <er.mohak97@gmail.com>
2022-12-19 11:07:55 +0530

    day5

--- tripadvisor/src/components/HotelItem.jsx
+++ tripadvisor/src/components/HotelItem.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { NavLink } from "react-router-dom"
 function HotelItem({image,Title,price}) {
+  
   return (
    
     <div style={{width:"90%"}}>
      <NavLink to="/Hotels">
      <img src={image} style={{marginBottom:"40px"}}></img>
-     <h3 style={{marginRight:"280px",marginBottom:"20px"}}>{Title}</h3>
-     <p style={{marginRight:"280px",marginBottom:"20px"}}>{price}</p>
+     <h3 >{Title}</h3>
+     <p >{price}</p>
      </NavLink>
      
      
